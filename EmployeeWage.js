e97f01b8e814f06d01e144abe2cbb6c9a0008a49
Naincy224 <pihusingh224@gmail.com>
2022-04-11 16:49:04 +0530

    Added:Employee is present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,2 +1,11 @@
 //Start welcome message
-console.log("Welcome to Employee Wage Computation Program");
+console.log("****Welcome to Employee Wage Computation Program****");
+console.log();
+//UC1 check employee is present or absent
+var Attendence = Math.random();
+console.log(Attendence);
+if(Attendence < 0.5){
+    console.log("Employee is Absent")
+}else{
+    console.log("Employee is Present")
+}
